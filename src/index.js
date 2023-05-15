523e499507991acc40f363ae85ff349f8951f767
马振东 <mazd@unuo.com>
2023-05-15 16:57:54 +0800

    17. JSX 综合案例

--- src/index.js
+++ src/index.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import './index.css'
-import classNames from 'classnames'
 
+const comments = [
+    {
+        id: 100,
+        name: '__RichMan',
+        avatar: 'https://r1.ykimg.com/051000005BB36AF28B6EE4050F0E3BA6',
+        content: '这阵容我喜欢😍靳东&闫妮，就这俩名字，我就知道是良心剧集...锁了🔒',
+        time: '2021/10/12 10:10:23',
+        vip: true,
+    },
+    {
+        id: 101,
+        name: '糖蜜甜筒颖',
+        avatar:
+            'https://image.9xsecndns.cn/image/uicon/712b2bbec5b58d6066aff202c9402abc3370674052733b.jpg',
+        content:
+            '突围神仙阵容 人民的名义第三部来了 靳东陈晓闫妮秦岚等众多优秀演员实力派 守护人民的财产 再现国家企业发展历程',
+        time: '2021/09/23 15:12:44',
+        vip: false,
+    },
+    {
+        id: 102,
+        name: '大星的妈妈',
+        avatar: 'https://static.youku.com/lvip/img/avatar/310/6.png',
+        content:
+            '第一集看的有点费力，投入不了，闫妮不太适合啊，职场的人哪有那么多表情，一点职场的感觉都没有',
+        time: '2021/07/01 00:30:51',
+        vip: true,
+    },
+];
 
-
-// 2. 数据
-const isActive = true;
-const isBlock = true;
-
-// 3. 产生类名
-const className = classNames('button', {
-    active: isActive,
-    block: isBlock,
-});
-
-const element = <span className={className}>按钮</span>;
-
-
+const element = (
+    <div className="comments">
+        <h3 className="comm-head">
+            热门评论<sub>({comments.length})</sub>
+        </h3>
+        <ul className="comm-list">
+            {comments.map((item,index) => {
+                return (
+                    <li key={index} className="comm-item">
+                        <div
+                            className="avatar"
+                            style={{ backgroundImage: `url(${item.avatar})` }}
+                        ></div>
+                        <div className="info">
+                            <p className={`name ${item.vip ? 'vip' : ''}`}>
+                                {item.name}
+                                {item.vip ? (
+                                    <img src="https://gw.alicdn.com/tfs/TB1c5JFbGSs3KVjSZPiXXcsiVXa-48-48.png" />
+                                ) : null}
+                            </p>
+                            <p className="time">{item.time}</p>
+                            <p>{item.content}</p>
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(
-    element
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(element);
