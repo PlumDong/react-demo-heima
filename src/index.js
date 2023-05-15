5400beece81d9fe82b91663e3cdf1f4e2eb04240
马振东 <mazd@unuo.com>
2023-05-15 09:47:37 +0800

    9. JSX 嵌入表达式

--- src/index.js
+++ src/index.js
@@ -4,12 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// 数据
+const data = {
+    name: 'tom',
+    age: 18,
+};
+
+// 函数
+const up = () => {
+    return data.name.toUpperCase();
+};
+
+// jsx表达式
+const list = (
+    <ul>
+        <li>jack</li>
+        <li>tony</li>
+    </ul>
+);
+
 const element = (
-    <div className="box">
-        <label htmlFor="ck"></label>
-        <input id="ck" type="checkbox" />
+    <div>
+         {/*1. 使用数据  注释推荐快键键(ctrl+/)  */}
+        <div>姓名：{data.name}</div>
+        <div>年龄：{data.age}</div>
+        {/* 2. 使用运算 */}
+        <div>明年几岁：{data.age + 1}</div>
+        {/* 3. 使用三元 */}
+        <div>是否成年：{data.age > 16 ? '是' : '否'}</div>
+        {/* 4. 使用函数 */}
+        <div>姓名大写：{up()}</div>
+        {/* 5. 使用JSX(jsx也是表达式) */}
+        <div>朋友：{list}</div>
     </div>
 );
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     element
