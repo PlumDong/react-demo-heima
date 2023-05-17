c2d8f344cf2d2b4168e6a5392283c3a104588e65
马振东 <mazd@unuo.com>
2023-05-17 15:16:05 +0800

    4. 组件通讯-父传子方式

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Component} from "react";
+import {render} from "@testing-library/react";
 
-function Child(props) {
-    return (
-        <div>
-            <h3>子组件：{props.money}</h3>
-        </div>
-    );
-}
-class Parent extends Component {
-    state = {
-        money: 10000,
-    };
-    render() {
-        return (
-            <div>
-                <h1>父组件：{this.state.money}</h1>
-                <Child money={this.state.money} />
+function App() {
+    return (<div className="parent">
+            <h1>今日要闻</h1>
+            <div className="child">
+                <h3>坚定不移走中国特色社会主义法治道路</h3>
+                <div className="detail">
+                    <span>新华社</span>
+                    <span>774点赞</span>
+                </div>
             </div>
-        );
-    }
+            <div className="child">
+                <h3>2021年度法治人物——倪伯苍 </h3>
+                <div className="detail">
+                    <span>央视网</span>
+                    <span>774点赞</span>
+                </div>
+            </div>
+            <div className="child">
+                <h3>岁末年终 愿这份“温良”伴你乘风破浪 </h3>
+                <div className="detail">
+                    <span>央视新闻客户端</span>
+                    <span>248点赞</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(
-    <Parent>
+root.render(<App>
 
-    </Parent>
-);
+</App>);
