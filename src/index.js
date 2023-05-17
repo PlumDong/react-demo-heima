da14f901851ddcdf35dffb47e55ddbd16837ece8
马振东 <mazd@unuo.com>
2023-05-17 15:13:33 +0800

    组件通讯-父传子方式

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-// 定义组件 props包含{name:'jack',age:'20'}
-function Hello(props) {
-    return <div>接收到数据: {props.name}  {props.age}</div>;
+import {Component} from "react";
+
+function Child(props) {
+    return (
+        <div>
+            <h3>子组件：{props.money}</h3>
+        </div>
+    );
+}
+class Parent extends Component {
+    state = {
+        money: 10000,
+    };
+    render() {
+        return (
+            <div>
+                <h1>父组件：{this.state.money}</h1>
+                <Child money={this.state.money} />
+            </div>
+        );
+    }
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Hello name="jack" age="20" />);
+root.render(
+    <Parent>
+
+    </Parent>
+);
