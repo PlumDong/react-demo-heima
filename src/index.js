164a5688cb90732d77290177f31f499c44380023
马振东 <mazd@unuo.com>
2023-05-17 15:50:39 +0800

    6. 组件通讯-子传父方式

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import {render} from "@testing-library/react";
-
-function App() {
-    return (<div className="parent">
-            <h1>今日要闻</h1>
-            <div className="child">
-                <h3>坚定不移走中国特色社会主义法治道路</h3>
-                <div className="detail">
-                    <span>新华社</span>
-                    <span>774点赞</span>
-                </div>
+import {Component} from "react";
+class Parent extends Component {
+    state = {
+        money: 10000,
+    };
+    // 回调函数
+    buyPhone = (price) => {
+        this.setState({
+            money: this.state.money - price,
+        });
+    };
+    render() {
+        const { money } = this.state;
+        return (
+            <div>
+                <h1>父组件：{money}</h1>
+                <Child money={money} buyPhone={this.buyPhone} />
             </div>
-            <div className="child">
-                <h3>2021年度法治人物——倪伯苍 </h3>
-                <div className="detail">
-                    <span>央视网</span>
-                    <span>774点赞</span>
-                </div>
-            </div>
-
-            <div className="child">
-                <h3>岁末年终 愿这份“温良”伴你乘风破浪 </h3>
-                <div className="detail">
-                    <span>央视新闻客户端</span>
-                    <span>248点赞</span>
-                </div>
-            </div>
-        </div>
-    )
+        );
+    }
 }
-
+const Child = (props) => {
+    const handleClick = () => {
+        // 子组件调用父组件传递过来的回调函数
+        props.buyPhone(5000);
+    };
+    return (
+        <div>
+            <h3>子组件：{props.money}</h3>
+            <button onClick={handleClick}>买手机</button>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App>
-
-</App>);
+root.render(<Parent></Parent>);
