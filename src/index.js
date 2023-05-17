d2414541257e57477dffdc9330ba6f185d19692b
马振东 <mazd@unuo.com>
2023-05-17 11:33:08 +0800

    2. 组件通讯-props 基本使用

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./App";
+// 定义组件 props包含{name:'jack',age:'20'}
+function Hello(props) {
+    return <div>接收到数据: {props.name}  {props.age}</div>;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App></App>);
+root.render(<Hello name="jack" age="20" />);
