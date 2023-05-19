cccf8982aadc0468125f121fa0ceb275a2bfddb8
马振东 <mazd@unuo.com>
2023-05-19 16:57:02 +0800

    1. props-类型校验

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
-// 定义组件
-const Hello = (props) => {
-    return <div>该组件的子节点：{props.children}</div>;
-};
-// 使用组件
+import PropTypes from 'prop-types'
 
+const List = props => {
+    const arr = props.colors
+    const lis = arr.map((item, index) => <li key={index}>{item.name}</li>)
+    return <ul>{lis}</ul>
+}
+
+List.propTypes = {
+    // props属性：校验规则
+    colors: PropTypes.array
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Hello>我是子节点</Hello>);
+root.render(List({colors:[{name:"张三"},{name:"李四"}]}));
