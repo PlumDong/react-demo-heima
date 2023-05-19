652a1b3f467b9c8417c587473c4073274475026f
马振东 <mazd@unuo.com>
2023-05-19 16:49:53 +0800

    16. props-children 属性

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
+// 定义组件
+const Hello = (props) => {
+    return <div>该组件的子节点：{props.children}</div>;
+};
+// 使用组件
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App></App>);
+root.render(<Hello>我是子节点</Hello>);
