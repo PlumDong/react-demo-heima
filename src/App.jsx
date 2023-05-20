6430cebdf0148e61d7e141eac118bb850578706f
马振东 <mazd@unuo.com>
2023-05-20 14:56:45 +0800

    11. setState扩展-更多用法

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,34 @@ export default class Demo extends Component {
         count: 0
     }
     handleClick = () => {
-        // this.setState({count: this.state.count+1})
-        // this.setState({count: this.state.count+1})
-        // 页面展示 1
         this.setState(prevState=>{
             return {
                 count: prevState.count + 1
             }
+        },()=>{
+            console.log('更新后1：', this.state.count)  // 打印：1
         })
         this.setState(prevState=>{
             return {
                 count: prevState.count + 1
             }
+        },()=>{
+            console.log('更新后2：', this.state.count)  // 打印：1
         })
-
         this.setState(prevState=>{
             return {
                 count: prevState.count + 1
             }
+        },()=>{
+            console.log('更新后3：', this.state.count)  // 打印：1
         })
-        // 页面展示 3
+        console.log('未更新：', this.state.count)  // 打印：0
     }
     render() {
         return (
             <div>
                 <div>Demo组件：{this.state.count}</div>
-                <button onClick={this.handleClick}>setState串联更新数据</button>
+                <button onClick={this.handleClick}>setState更新后执行逻辑</button>
             </div>
         )
     }
