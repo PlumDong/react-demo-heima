dda9d6a37c15efe970e04d8a5f1a9ec3fbb6c202
马振东 <mazd@unuo.com>
2023-05-20 14:38:15 +0800

    10. setState扩展-发现问题

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,19 @@
-import {Component} from 'react';
-import CommentInput from "./components/CommentInput";
-import CommentList from "./components/CommentList";
-import CommentHead from "./components/CommentHead";
-
-export default class App extends Component {
+import React, {Component} from 'react'
+export default class Demo extends Component {
     state = {
-        // 用户信息
-        user: {
-            name: '清风徐来',
-            avatar: 'https://static.youku.com/lvip/img/avatar/310/6.png',
-            vip: true,
-        },
-        // 评论列表
-        comments: [
-            {
-                id: 100,
-                name: '__RichMan',
-                avatar: 'https://r1.ykimg.com/051000005BB36AF28B6EE4050F0E3BA6',
-                content:
-                    '这阵容我喜欢😍靳东&闫妮，就这俩名字，我就知道是良心剧集...锁了🔒',
-                time: new Date('2021/10/12 10:10:23'),
-                vip: true,
-                collect: false,
-            },
-            {
-                id: 102,
-                name: '清风徐来',
-                avatar: 'https://static.youku.com/lvip/img/avatar/310/6.png',
-                content:
-                    '第一集看的有点费力，投入不了，闫妮不太适合啊，职场的人哪有那么多表情，一点职场的感觉都没有',
-                time: new Date('2021/07/01 00:30:51'),
-                vip: true,
-                collect: false,
-            },
-            {
-                id: 101,
-                name: '糖蜜甜筒颖',
-                avatar:
-                    'https://image.9xsecndns.cn/image/uicon/712b2bbec5b58d6066aff202c9402abc3370674052733b.jpg',
-                content:
-                    '突围神仙阵容 人民的名义第三部来了 靳东陈晓闫妮秦岚等众多优秀演员实力派 守护人民的财产 再现国家企业发展历程',
-                time: new Date('2021/09/23 15:12:44'),
-                vip: false,
-                collect: true,
-            },
-        ],
-        active: 'default',
-    };
-    setActive = value => {
-        // 改变过排序修改状态
-        if (value !== this.state.active) {
-            this.setState({active: value})
-        }
+        count: 0
     }
-    addComment = (content) => {
-        // 递增ID
-        const id = Math.max(...this.state.comments.map((item) => item.id)) + 1;
-        const newComment = {
-            ...this.state.user,
-            id,
-            content,
-            time: new Date(),
-            collect: false,
-        };
-        this.setState({
-            comments: [...this.state.comments, newComment],
-        });
-    };
-    // 删除评论
-    delComment = (id) => {
-        this.setState({
-            comments: this.state.comments.filter((item) => item.id !== id),
-        });
-    };
-// 收藏评论&取消收藏
-    collectComment = (id) => {
-        this.setState({
-            comments: this.state.comments.map((item) => {
-                if (item.id === id) {
-                    return {...item, collect: !item.collect};
-                } else {
-                    return item;
-                }
-            }),
-        });
-    };
-
+    handleClick = () => {
+        this.setState({count: this.state.count+100})
+        this.setState({count: this.state.count+1})
+        console.log(this.state.count)  // 打印0
+    }
     render() {
-        const {user, comments, active} = this.state
+        console.log('render')
         return (
-            <div className="comments">
-                {/* 输入框组件 */}
-                <CommentInput addComment={this.addComment}/>
-                {/* 标题排序组件 */}
-                <CommentHead active={active} setActive={this.setActive} comments={comments}/>
-                {/* 列表组件 */}
-                {/*<CommentList />*/}
-                <CommentList
-                    user={user}
-                    comments={comments}
-                    active={active}
-                    delComment={this.delComment}
-                    collectComment={this.collectComment}/>
+            <div>
+                <div>Demo组件：{this.state.count}</div>
+                <button onClick={this.handleClick}>体现“异步”和合并</button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Component} from "react";
-
-class Demo extends Component {
-    constructor () {
-        super()
-        console.log('1. constructor执行')
-    }
-    componentDidMount () {
-        console.log('3. componentDidMount执行')
-    }
-    render() {
-
-        console.log('2. render执行')
-        return <div>{'sss' ?? 'App组件'}</div>
-    }
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Demo />);
+root.render(  <App />,);
