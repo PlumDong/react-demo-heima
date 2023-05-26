0022fe95e17ad577ccbbcaaffa498828029e07a6
马振东 <mazd@unuo.com>
2023-05-26 14:09:12 +0800

    19. 商品列表渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-import {CartHeader} from "./components/CartHeader";
-import CartFooter from "./components/CartFooter";
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 import {GoodsItem} from "./components/GoodsItem";
 
+const http = axios.create({
+    baseURL: 'http://localhost:8888',
+});
+
 export default function App() {
+    const [goodsList, setGoodsList] = useState([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const res = await http.get('/goodsList');
+            setGoodsList(res.data);
+        };
+        loadData();
+    }, []);
+
     return (
         <div className="app">
-            <CartHeader>购物车</CartHeader>
-            <GoodsItem />
-            <GoodsItem />
-            <GoodsItem />
-            <CartFooter />
+            {/* 商品列表项 */}
+            {goodsList.map((item) => (
+                <GoodsItem key={item.id} {...item} />
+            ))}
         </div>
     );
 }
--- src/components/GoodsItem/index.jsx
+++ src/components/GoodsItem/index.jsx
@@ -1,22 +1,29 @@
-import './index.scss';
-export const GoodsItem = () => {
+export const GoodsItem = ({
+                              goods_count,
+                              goods_img,
+                              goods_name,
+                              goods_price,
+                              goods_state,
+                              id,
+                          }) => {
     return (
-        <div className="cart-goods-item">
+        <div className="my-goods-item">
             <div className="left">
                 <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="input" />
-                    <label className="custom-control-label" htmlFor="input">
-                        <img
-                            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-                            alt=""
-                        />
+                    <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id={`input-${id}`}
+                    />
+                    <label className="custom-control-label" htmlFor={`input-${id}`}>
+                        <img src={goods_img} alt="" />
                     </label>
                 </div>
             </div>
             <div className="right">
-                <div className="top">商品名称</div>
+                <div className="top">{goods_name}</div>
                 <div className="bottom">
-                    <span className="price">¥ 商品价格</span>
+                    <span className="price">¥ {goods_price}</span>
                     <span>counter组件</span>
                 </div>
             </div>
