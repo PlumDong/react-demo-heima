9323e665ce1f34c9b57ce021f83ca86eab0942c1
马振东 <mazd@unuo.com>
2023-06-10 16:31:22 +0800

    4. 配置React-Redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import store from './store'
 import App from './App'
 import ReactDOM from 'react-dom'
 
-// eslint-disable-next-line no-undef
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('#root')
+    document.querySelector('#root')
 )
