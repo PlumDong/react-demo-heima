8073d82df6156baa349bd3c9452a36fe6bafe36c
马振东 <mazd@unuo.com>
2023-06-13 15:03:40 +0800

    黑马-redux-入门案例

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,26 @@
 import store from "./store";
 import {sendAction} from "./action";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+    const [value, setValue] = useState(store.getState().value);
+
     const handleClick = ()=>{
         const  v = sendAction()
-
         console.log("Home = ",v)
         store.dispatch(v)
     }
+    useEffect( ()=>{
+        store.subscribe((k)=>{
+            console.log('subscribe = ',store.getState());
+            setValue(store.getState().value)
+        })
+    } , [] )
 
     return (
         <>
         <button onClick={handleClick}>发送一个action</button>
-        <div>{store.getState().value}</div>
+        <div>{value}</div>
         </>
 
     );
