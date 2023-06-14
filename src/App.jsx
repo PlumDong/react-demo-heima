060061c3084637f3b5944554a092d0a28ca7eea0
马振东 <mazd@unuo.com>
2023-06-14 17:12:14 +0800

    综合案例

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./Home";
+import Boy from "./pages/boy";
 
 
 export default function App() {
     return (
         <div className='app'>
-            <div className='boy'></div>
+            <div className='boy'><Boy></Boy></div>
             <div className='girl'></div>
         </div>
     );
--- src/pages/boy/index.js
+++ src/pages/boy/index.js
@@ -1,11 +1,20 @@
 
 import img from "../../assets/fashe.jpg"
+import sending from "../../assets/sendimg.jpg"
+import { useState} from "react";
 
 
 const Boy = (props) => {
-  return(
+    const [isSend,setIsSend] = useState(false)
+
+    const handleClick = ()=> {
+        setIsSend(!isSend)
+    }
+
+    return(
       <div>
-        <img src={img} alt=''/>
+        <img src={isSend?sending:img} alt=''/>
+          <button onClick={handleClick}>{isSend?'停止发射':'发射爱心'}</button>
       </div>
   )
 }
