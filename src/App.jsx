55e89dc805c99daffc16ec79fab6a2b2b73fcc09
马振东 <mazd@unuo.com>
2023-06-15 10:13:31 +0800

    4. React路由-Route组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const First = () => <p>页面一的页面内容</p>
 const Tow = () => <p>页面二的页面内容</p>
+const All = () => <p>没有路由的页面内容</p>
 
 const App = () => {
     return (
@@ -22,8 +23,9 @@ const App = () => {
                   component属性：展示的组件
                   Route 组件写在哪，渲染出来的组件就展示在哪
                 */}
-                <Route path="/first" component={First}></Route>
+                <Route path="/first"><First></First></Route>
                 <Route path="/tow" component={Tow}></Route>
+                <Route component={All}></Route>
             </div>
         </Router>
     )
