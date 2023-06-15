c1265b086f5015f1d75981349736789d8ede43c2
马振东 <mazd@unuo.com>
2023-06-15 10:18:06 +0800

    5. React路由-路由匹配模式

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 const First = () => <p>页面一的页面内容</p>
 const Tow = () => <p>页面二的页面内容</p>
 const All = () => <p>没有路由的页面内容</p>
+const Home = () => <p>首页</p>
+const Login = () => <p>登录页</p>
 
 const App = () => {
     return (
@@ -13,8 +15,7 @@ const App = () => {
                         {/*
                         to属性：浏览器地址栏中的pathname（location.pathname）
                             */}
-                        <Link to="/first">页面一</Link>
-                        <Link to="/tow">页面二</Link>
+                        <Link to="/login">登录页面</Link>
                     </li>
                 </ul>
 
@@ -23,9 +24,9 @@ const App = () => {
                   component属性：展示的组件
                   Route 组件写在哪，渲染出来的组件就展示在哪
                 */}
-                <Route path="/first"><First></First></Route>
-                <Route path="/tow" component={Tow}></Route>
-                <Route component={All}></Route>
+                <Route path="/" component={Home} />
+                <Route path="/login" component={Login} />
+
             </div>
         </Router>
     )
