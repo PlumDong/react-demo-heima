5523dc3e63d3fe9f2c988abc6643f74e82bbdee7
马振东 <mazd@unuo.com>
2023-06-15 10:05:49 +0800

    1. React路由-基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from "react";
-import Home from "./Home";
-import Boy from "./pages/boy";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
+const First = () => <p>页面一的页面内容</p>
+const Tow = () => <p>页面二的页面内容</p>
 
-export default function App() {
+const App = () => {
     return (
-        <div className='app'>
-            <div className='boy'><Boy></Boy></div>
-            <div className='girl'></div>
-        </div>
-    );
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        {/*
+                        to属性：浏览器地址栏中的pathname（location.pathname）
+                            */}
+                        <Link to="/first">页面一</Link>
+                        <Link to="/tow">页面二</Link>
+                    </li>
+                </ul>
+
+                {/*
+                  path属性：路由规则
+                  component属性：展示的组件
+                  Route 组件写在哪，渲染出来的组件就展示在哪
+                */}
+                <Route path="/first" component={First}></Route>
+                <Route path="/tow" component={Tow}></Route>
+            </div>
+        </Router>
+    )
 }
+export default App;
