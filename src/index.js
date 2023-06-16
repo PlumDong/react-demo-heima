34230f42778858b29f334c49f5b3be908ef98d19
马振东 <mazd@unuo.com>
2023-06-16 17:23:53 +0800

    初始化 PC 极客园 项目

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import { Provider } from 'react-redux'
-import store from './store'
 import App from './App'
 import ReactDOM from 'react-dom'
 import './index.scss'
 
 
 ReactDOM.render(
-    <Provider store={store}>
         <App />
-    </Provider>,
-    document.querySelector('#root')
+    ,document.querySelector('#root')
 )
