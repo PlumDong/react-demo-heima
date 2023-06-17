ae4c5a1df4fe3f56a7a27d11112f6ecc6dd1da2c
马振东 <mazd@unuo.com>
2023-06-17 15:46:56 +0800

    初始化 PC 极客园 项目

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+// 导入路由
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
-const App = () => {
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+
+// 配置路由规则
+function App() {
     return (
-        <div className="app"></div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/home" component={Layout}></Route>
+                    <Redirect path="/" to="/home" />
+                </Switch>
+            </div>
+        </Router>
     )
 }
-export default App;
+
+export default App
