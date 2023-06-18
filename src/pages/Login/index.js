f9c5126696979ea4afe4ea472529825a240cc6b1
mzd <1695165447@qq.com>
2023-06-18 22:24:49 +0800

    2023-06-18

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,24 @@
-import logo from '../../assets/logo.png'
-import './index.scss'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 const Login = () => {
     return (
-        <div className="login">
-            <div className="login-container">
-                <img className="login-logo" src={logo} alt="" />
-                {/* 登录表单 */}
-            </div>
-        </div>
+        <Form size="large">
+            <Form.Item>
+                <Input prefix={<UserOutlined />} placeholder="请输入手机号" />
+            </Form.Item>
+            <Form.Item>
+                <Input prefix={<LockOutlined />} placeholder="请输入验证码" />
+            </Form.Item>
+            <Form.Item>
+                <Checkbox>我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" block>
+                    登录
+                </Button>
+            </Form.Item>
+        </Form>
     )
 }
-
-export default Login
+export default Login;
